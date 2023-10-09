c53a5c8a5232e3ed8dfaed62f95ff441036f2798
Jakub Mamala <g00408184@atu.ie>
2023-10-09 21:38:30 +0100

    made a new class for data storage and generated a constructor

---
+++ src/main/java/ie/atu/week1hw/DataStorage.java
@@ -0,0 +1,11 @@
+package ie.atu.week1hw;
+
+public class DataStorage {
+    private float result;
+    private String sumoperator;
+
+    public DataStorage(float result, String sumoperator) {
+        this.result = result;
+        this.sumoperator = sumoperator;
+    }
+}
