03b16716e104d9115f4d1405e8a8fbd898afa50c
Jakub Mamala <g00408184@atu.ie>
2023-10-09 22:20:32 +0100

    changed the case to an easier format

--- src/main/java/ie/atu/week1hw/calculateController.java
+++ src/main/java/ie/atu/week1hw/calculateController.java
@@ -1,30 +1,47 @@
 package ie.atu.week1hw;
 
-import  org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class calculateController {
-    private float result;
-    private String sumoperator;
+
+
     @GetMapping("/calculate")
-    public String calculation( @RequestParam("num1") int num1,
-                             @RequestParam("num2") int num2)
-    {
-    switch (sumoperator){
-        case "add": new calculateController(num1+num2,sumoperator);
-        case "subtrat":new calculateController(num1-num2,sumoperator);
-        case "multiply":new calculateController(num1*num2,sumoperator);
-        case "divide":new calculateController(num1/num2,sumoperator);
+    public String calculation(
+            @RequestParam("num1") int num1,
+            @RequestParam("num2") int num2,
+            @RequestParam("sumOperator") String sumOperator) {
 
-    }
+        double result = 0.0; // Initialize the result variable
 
-
-
-           // return;
+        switch (sumOperator) {
+            case "add":
+                result = num1 + num2;
+                break; // Don't forget to break after each case
+            case "subtract":
+                result = num1 - num2;
+                break;
+            case "multiply":
+                result = num1 * num2;
+                break;
+            case "divide":
+                if (num2 != 0) { // Check for division by zero
+                    result = (double) num1 / num2;
+                } else {
+                    return "Division by zero is not allowed.";
+                }
+                break;
+            default:
+                return "Invalid operator: " + sumOperator;
         }
 
+        // Create a DataStorage object with the result and sumOperator
+        DataStorage dataStorage = new DataStorage(result, sumOperator);
+
+        return dataStorage.toString();
     }
+
+}
 
