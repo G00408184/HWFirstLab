1875f9b6e84db397cac5109d68b198642bfbe2df
Jakub Mamala <g00408184@atu.ie>
2023-10-09 21:35:47 +0100

    added another requestparam called sumoperator

---
+++ src/test/java/ie/atu/week1hw/Week1HwApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.week1hw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week1HwApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
